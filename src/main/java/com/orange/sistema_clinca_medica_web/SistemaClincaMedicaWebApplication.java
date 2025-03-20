a693e720b2037548b5a3e9d11a169e7a6edb91a6
RogerSouza1 <roger.7090@outlook.com>
2025-03-19 22:01:12 -0300

    initialize Spring Boot application with Maven wrapper and basic configuration

---
+++ src/main/java/com/orange/sistema_clinca_medica_web/SistemaClincaMedicaWebApplication.java
@@ -0,0 +1,13 @@
+package com.orange.sistema_clinca_medica_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaClincaMedicaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaClincaMedicaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/sistema_clinca_medica_web/SistemaClincaMedicaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.sistema_clinca_medica_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaClincaMedicaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
